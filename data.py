7719e654fd8dc0094f19c01f5db77db5ca968312
Duncan-DS <schwartzanatar17@gmail.com>
2024-06-17 10:31:39 +0200

    created the data lists for the genres

---
+++ data.py
@@ -0,0 +1,153 @@
+def genres() :
+    def action() :
+        return [
+            {"name": "God of War", "developer": "Santa Monica Studios", "Publisher": "Playstation Publishing LLC", "Price": 719}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def adventure() :
+        return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def rpg() :
+        return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def stratergy() :
+         return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def simulation() :
+         return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def sports() :
+         return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def horror() :
+         return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def sandbox() :
+          return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def fighter() :
+          return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def puzzle() :
+          return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def racing() :
+          return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def shooter() :
+          return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def mmorpg() :
+          return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def rtstratergy() :
+          return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def survivahorror() :
+          return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def battleroyale() :
+          return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
+
+    def stealth() :
+          return [
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+            {"name": "", "developer": "", "Publisher": "", "Price":}
+        ]
